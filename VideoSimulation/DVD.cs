b767cee60abd45054716fdd536a23db0e29e059f
Testo12 <erik.malmsten99@gmail.com>
2018-11-05 15:18:30 +0100

    Added DVD structure

---
+++ VideoSimulation/DVD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoSimulation
+{
+   
+    public class DVD : IOperate
+    {
+        public int id { get; set; }
+
+        public void Play()
+        {
+            id = 2;
+            
+        }
+
+        public void Stop()
+        {
+            id = 3;
+            
+        }
+    }
+}
