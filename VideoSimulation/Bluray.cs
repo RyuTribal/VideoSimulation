54c347fbaacce830cfa64ef44ab86db859a11796
WilleAsander <wasander13@gmail.com>
2018-11-05 15:35:05 +0100

    Added BluRay to the code

---
+++ VideoSimulation/Bluray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoSimulation
+{
+    class Bluray : IOperate
+    {
+        
+        public string Play()
+        {
+            return "BluRay is playing";   
+        }
+
+        public string Stop()
+        {
+            return "BluRay stopped playing";
+        }
+    }
+}
